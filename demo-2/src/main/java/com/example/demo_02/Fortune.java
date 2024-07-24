175d4dab831357b666956655bcec3c2b5da44aaf
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-07-24 09:26:00 +0900

    fortune java

---
+++ demo-2/src/main/java/com/example/demo_02/Fortune.java
@@ -0,0 +1,25 @@
+package com.example.demo_02;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class Fortune 
+{
+	@GetMapping( "/fortune" )
+	public String start() {
+		double fn = Math.random();		//0.0～1.0までの乱数を生成
+		if( fn >= 0.7 ) {
+			return( "greatFortune.html" );
+		}else if ( fn >= 0.4 ) {
+			return( "middleFortune.html" );
+		}else if ( fn >= 0.1 ) {
+			return( "smallFortune.html" );
+		}else{
+			return( "misFortune.html" );	
+		}
+				
+	}
+				
+}
